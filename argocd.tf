f87badda0e68054ff1a273df39b825d897addc1b
barry.pekerman <barrypek@gmail.com>
2025-12-02 05:14:23 +0200

    Refactor: Switch to whitelist .gitignore and remove AGENTS.md

--- argocd.tf
+++ argocd.tf
@@ -13,49 +13,41 @@ resource "helm_release" "argocd" {
           type = "LoadBalancer"
         }
       }
+      extraObjects = [
+        {
+          apiVersion = "argoproj.io/v1alpha1"
+          kind       = "Application"
+          metadata = {
+            name      = "root-app"
+            namespace = "argocd"
+            finalizers = [
+              "resources-finalizer.argocd.argoproj.io"
+            ]
+          }
+          spec = {
+            project = "default"
+            source = {
+              repoURL        = "https://github.com/BarryPekerman/instant-infra.git"
+              targetRevision = "main"
+              path           = "gitops/apps"
+            }
+            destination = {
+              server    = "https://kubernetes.default.svc"
+              namespace = "argocd"
+            }
+            syncPolicy = {
+              automated = {
+                prune    = true
+                selfHeal = true
+              }
+            }
+          }
+        }
+      ]
     })
   ]
 
   depends_on = [
     aws_eks_node_group.system
-  ]
-}
-
-# Bootstrap the Root App
-resource "helm_release" "argocd_apps" {
-  name             = "argocd-apps"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argocd-apps"
-  version          = "1.6.2"
-  namespace        = "argocd"
-
-  values = [
-    yamlencode({
-      applications = {
-        root-app = {
-          namespace = "argocd"
-          project   = "default"
-          source = {
-            repoURL        = "https://github.com/BarryPekerman/instant-infra.git"
-            targetRevision = "main"
-            path           = "gitops/apps"
-          }
-          destination = {
-            server    = "https://kubernetes.default.svc"
-            namespace = "argocd"
-          }
-          syncPolicy = {
-            automated = {
-              prune    = true
-              selfHeal = true
-            }
-          }
-        }
-      }
-    })
-  ]
-
-  depends_on = [
-    helm_release.argocd
   ]
 }
